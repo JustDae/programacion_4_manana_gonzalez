49bc21b54565d65acd16aef046f365a25cd7a2a2
hyxuu <hyxuu05@gmail.com>
2026-06-01 19:37:19 -0500

    mp added to main

---
+++ modulo_1/basics/mp/05_readline_mp.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // ReadLine
+    println("Escribe tu ruta")
+    val ruta = readLine() ?: "anonimo"
+    println("Ruta incluida : $ruta")
+    
+    println("Escribe destino")
+    val destino = readLine() ?: "anonimo"
+    println("Destino incluido : $destino")
+    
+}
